b28f0419a3f22996c6f6228b684641d53e1ce52a
GuyGreen1992 <guygreen873@gmail.com>
2018-06-17 18:58:27 +0300

    method decleration update

--- src/oop/ex6/main/MethodScope.java
+++ src/oop/ex6/main/MethodScope.java
@@ -16,32 +16,46 @@ public class MethodScope extends Scope{
 
     ArrayList<Variable> argList;
 
-    public MethodScope(LineNode root, Scope parent, String name){
+    public MethodScope(LineNode root, Scope parent) throws ExceptionFileFormat {
         super(root, parent);
-        this.name = name;
-        argList = new ArrayList<>();
+        methodDecAnalyzer(root.data);
     }
 
     private void methodDecAnalyzer(String deceleration) throws ExceptionFileFormat {
-        String methodName;
-        MethodScope currMethod;
-        ArrayList<Variable> argList;
         Pattern begDeclarePattern = Pattern.compile(METHODS_FIRST_PART_REGEX);
         Pattern endDeclarePattern = Pattern.compile(METHODS_SECOND_PART_REGEX);
         Matcher begDeclareMatcher = begDeclarePattern.matcher(deceleration);
         if(!begDeclareMatcher.find())
             throw new ExceptionFileFormat( ILLEGAL_METHOD_DECELERATION);
-        methodName = begDeclareMatcher.group(NAME_PLC);
-        currMethod = new MethodScope(methodName);
+        name = begDeclareMatcher.group(NAME_PLC);
         // todo check if returned index of method "end" includes last char
         Matcher endDeclareMatcher = endDeclarePattern.matcher(deceleration);
         if(!endDeclareMatcher.find())
             throw new ExceptionFileFormat( ILLEGAL_METHOD_DECELERATION);
         String slicedDeceleration = deceleration.substring(begDeclareMatcher.end() + 1, endDeclareMatcher.start()); //remove the "name part" of line
         argList = getArgsList(slicedDeceleration); // todo check which input the factory gets
-        currMethod.argList = argList;
-        methods.put(methodName, currMethod);
     }
 
+    void verifyMethod(){
+        //todo
+    }
+
+
+
+
+
+
+
+
+
+    // todo send to method of variable class or finish writing this method
+//    private ArrayList<Variable> getArgsList(String argsDeclare){
+//        ArrayList<Variable> argList = new ArrayList<>();
+//        String[] args = argsDeclare.split(COMMA);
+//        for(String arg : args){
+//            //argList.add(VaribleFactory.analyzeVariable(arg)); // todo change to explicit method call
+//        }
+//        return argList;
+//    }
 
 }
